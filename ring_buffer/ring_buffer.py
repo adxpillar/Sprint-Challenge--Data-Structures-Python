75d640a05072d6f56d080c3a76b9ad2d007cff4f
adxpillar <dsa.adeagbo@gmail.com>
2020-06-12 18:24:18 -0400

    add ringer first version

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.q = []
 
     def append(self, item):
-        pass
-
+        if len(self.q) < self.capacity:
+                self.q.append(item)
+                if len(self.q) == self.capacity:
+                    self.q.pop()
+                    self.q.append(item)
+                    
+                         
     def get(self):
-        pass
+        return self.q
+
+
+# A ring buffer is a non-growable buffer with a fixed size. When the ring buffer is 
+# full and a new element is inserted, the oldest element in the ring buffer is overwritten with the newest element. 
+# This kind of data structure is very useful for use cases such as storing logs and history information, 
+# where you typically want to store information up until it reaches a certain age, 
+# after which you don't care about it anymore and don't mind seeing it overwritten by newer data.
+
+
+
