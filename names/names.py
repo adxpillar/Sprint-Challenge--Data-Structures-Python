ec94ed856726fbf18ca593b601ab1da84d18d0b3
adxpillar <dsa.adeagbo@gmail.com>
2020-06-12 19:40:33 -0400

    add names list

--- names/names.py
+++ names/names.py
@@ -19,9 +19,7 @@ duplicates = []  # Return the list of duplicates in this data structure
 #             duplicates.append(name_1)
 
 
-       
-for n in [str(names_1),str(names_2)]:
-    duplicates.append(n)
+[duplicates.append(n) for n in names_1 if n in names_2]
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -31,3 +29,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
